27c98e1c68f588951e19e5acb82e1aede9aa7ee7
unknown <clintoncute04@gmail.com>
2026-05-25 03:37:23 +0100

    organize the support page and landing page

--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
-
-
+import Header from '../Components/Header'
+import LandingPageHero from '../Components/LandingPageHero'
+import LandingPageSubHero from '../Components/LandingPageSubHero'
+import Carousel from '../Components/Carousel'
+import Testimonials from '../Components/Testimonials'
+import HowItWorks from '../Components/HowItWorks'
+import LatestArticles from '../Components/LatestArticles'
+import PromoBanner from '../Components/PromoBanner'
+import Footer from '../Components/Footer'
 const LandingPage = () => {
   return (
     <div>
-   
-    
+   <Header/>
+    <LandingPageHero/>
+    <LandingPageSubHero/>
+    <Carousel/>
+    <HowItWorks/>
+    <Testimonials/>
+    <LatestArticles/>
+    <PromoBanner/>
+    <Footer/>
     </div>
   )
 }
